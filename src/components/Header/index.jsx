ffbbef5176a6ed16f675cc680676516574508b3e
sebastiao <sebastiaofdmn@gmail.com>
2023-05-24 21:42:20 -0300

    components

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout } from './styles';
+
+export function Header() {
+    return(
+        <Container>
+            <Profile>
+                <img src="https://github.com/sebastiaofelix.png" 
+                alt="Foto Usuario"/>
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Sebastiao Felix</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    );
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,18 +1,47 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
 
+import { Tags } from '../../components/Tags'
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
+
 
 export function Details(){
-
   return(
     <Container>
-    <h1>hello world!</h1>
-    <span>Seba</span>
+      <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota"/>
 
-    <Button  title="Login" loading />
-    <Button  title="Cadastrar"/>
-    <Button  title="Voltar"/>
+          <h1>Introdução ao React</h1>
 
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est, voluptatibus,
+             assumenda corporis adipisci vero facilis saepe ratione blanditiis aspernatur tempore
+              suscipit dolorum commodi velit rem molestias! Numquam, assumenda quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est, voluptatibus,
+             assumenda corporis adipisci vero facilis saepe ratione blanditiis aspernatur tempore
+              suscipit dolorum commodi velit rem molestias! Numquam, assumenda quos.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est, voluptatibus,
+             assumenda corporis adipisci vero facilis saepe ratione blanditiis aspernatur tempore
+              suscipit dolorum commodi velit rem molestias! Numquam, assumenda quos.
+          </p>
+
+          <Section title="Links Uteis">
+            <Links>
+              <li><a href="#">rocketseat.com.br</a></li>
+              <li><a href="#">rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tags title="express"/>
+            <Tags title="node.js"/>
+          </Section>
+
+          <Button  title="Voltar"/>            
+        </Content>
+      </main>
     </Container>
   )
 }
