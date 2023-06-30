fd16f8d96fca2731c185948cd15743118aad9c53
sebastiaofelix <sebastiaofdmn@gmail.com>
2023-06-30 16:20:40 -0300

    login e variáveis especiais

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
-import theme from './styles/theme';
+
+import { AuthProvider } from './hooks/auth';
 
 import { Routes } from './routes';
 
-
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
-      <GlobalStyle />  
-      <Routes />
+      <GlobalStyle />
+
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+        
     </ThemeProvider>
   </React.StrictMode>
 )
